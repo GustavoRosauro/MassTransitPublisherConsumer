97cfe3925e99d417e00b7302f429bf0b3cf94dd2
GustavoRosauro <gustavosrosauro@gmail.com>
2021-04-24 20:03:51 -0300

    Criado publicador e consumidor

---
+++ Consumer.Api/OrderConsumer.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using SharedModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Consumer.Api
+{
+    public class OrderConsumer : 
+        IConsumer<Order>
+    {
+        public async Task Consume(ConsumeContext<Order> context)
+        {
+            var data = context.Message;
+            using (var writter = new StreamWriter(@"C:\log\rabbit.txt",true))
+            {
+                writter.WriteLine($"Nome: {data.ProductName} Quantidade: {data.Qtd}");
+            }
+
+            await Task.CompletedTask;
+        }
+    }
+}
